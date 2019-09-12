b7e910033c557e3fe88ca75815f1234bc09044c0
mattisabrat <mdavenport@rockefeller.edu>
2019-09-13 00:03:48 +0300

    Restructured to be both a git repo and the Pypi upload.

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env/python3
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+from distutils.core import setup
+setup(
+  name = 'ProcessedPiRecorder',         
+  packages = ['ProcessedPiRecorder'],   
+  version = '0.1.0',      
+  license='MIT',        #https://help.github.com/articles/licensing-a-repository
+  description = 'Multiprocessed picamera class',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  author = 'Matthew Davenport',      
+  author_email = 'mdavenport@rockefeller.edu', 
+  url = 'https://github.com/mattisabrat/ProcessedPiRecorder',
+  install_requires=[            # I get to this in a second
+          'picamera["array"]',
+          'opencv-contrib-python ',
+          'tifffile',
+          'numpy'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)' ,
+    'Programming Language :: Python :: 3 :: Only',
+    'Operating System :: POSIX :: Linux'
+  ],
+)
