ecf0cb41cc7f52a90075c87823a4ede9642ef0b8
mattisabrat <mdavenport@rockefeller.edu>
2020-01-08 23:51:18 +0300

    more changes to setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
-#!/usr/bin/env/python3
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-from distutils.core import setup
-setup(
+import setuptools
+setuptools.setup(
   name = 'ProcessedPiRecorder',         
   packages = ['ProcessedPiRecorder'],   
   version = '0.2.0',      
-  license='GPLv3',        #https://help.github.com/articles/licensing-a-repository
+  license='GPLv3',        
   description = 'Multiprocessed picamera class for simpler and faster computer vision',
   long_description=long_description,
   long_description_content_type="text/markdown",
   author = 'Matthew Davenport',      
   author_email = 'mdavenport@rockefeller.edu', 
   url = 'https://github.com/mattisabrat/ProcessedPiRecorder',
-  install_requires=[            # I get to this in a second
-          'picamera["array"]==1.13',
+  install_requires=[            
+          'picamera==1.13',
           'opencv-contrib-python==3.4.4.19',
           'tifffile==2019.7.26',
           'numpy==1.17.0',
